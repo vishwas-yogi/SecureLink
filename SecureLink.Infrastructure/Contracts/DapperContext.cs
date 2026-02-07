d4540983517734ceede40cdbf28f3f07c5d5f6a9
vishwas-yogi <vishwas.yogi.swarnkar@gmail.com>
2026-02-07 14:15:39 +0000

    feat: add dapper and connecting to db

---
+++ SecureLink.Infrastructure/Contracts/DapperContext.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Data;
+using Microsoft.Extensions.Options;
+using Npgsql;
+
+namespace SecureLink.Infrastructure.Contracts;
+
+public class DapperContext : IDapperContext
+{
+    private readonly string _connectionString;
+
+    public DapperContext(IOptions<DapperOptions> options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+        _connectionString =
+            options.Value.ConnectionString
+            ?? throw new ArgumentException(
+                "ConnectionString must be provided in configuration",
+                nameof(options)
+            );
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        return new NpgsqlConnection(_connectionString);
+    }
+}
